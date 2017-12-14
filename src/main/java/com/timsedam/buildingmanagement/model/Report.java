61e91206e25a3eba6718e50130a3500cc8a55f5e
C0mpy <n.zunic@yahoo.com>
2017-12-14 13:53:33 +0100

    changed sql configs

--- src/main/java/com/timsedam/buildingmanagement/model/Report.java
+++ src/main/java/com/timsedam/buildingmanagement/model/Report.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -20,6 +21,7 @@ public class Report {
 	@ManyToOne
 	private User sender;
 	private String status;
+	@Lob
 	private String description;
 	@ManyToOne
 	private Building location;
