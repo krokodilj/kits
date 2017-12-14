57efe768e09ec7ed2acb300334bc2c0c82a479a9
sirko <diskozub@hotmail.com>
2017-12-14 14:56:44 +0100

    add resident to residence

--- src/main/java/com/timsedam/buildingmanagement/controller/ResidentController.java
+++ src/main/java/com/timsedam/buildingmanagement/controller/ResidentController.java
@@ -2,9 +2,9 @@ package com.timsedam.buildingmanagement.controller;
 
 import com.timsedam.buildingmanagement.dto.UserDTO;
 import com.timsedam.buildingmanagement.dto.UserRegisterDTO;
-import com.timsedam.buildingmanagement.model.Resident;
-import com.timsedam.buildingmanagement.model.Role;
-import com.timsedam.buildingmanagement.model.User;
+import com.timsedam.buildingmanagement.model.*;
+import com.timsedam.buildingmanagement.service.BuildingService;
+import com.timsedam.buildingmanagement.service.ResidenceService;
 import com.timsedam.buildingmanagement.service.RoleService;
 import com.timsedam.buildingmanagement.service.UserService;
 import org.modelmapper.ModelMapper;
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,6 +23,9 @@ public class ResidentController {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private ResidenceService residenceService;
 
     @Autowired
     private UserService userService;
@@ -51,7 +51,27 @@ public class ResidentController {
         resident.setRole(role);
         userService.save(resident);
 
-        UserDTO responseData = modelMapper.map(resident, UserDTO.class);
-        return new ResponseEntity(resident,HttpStatus.OK);
+        UserDTO residentDTO = modelMapper.map(resident, UserDTO.class);
+        return new ResponseEntity(residentDTO,HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{residentId}/add_to_residence/{residenceId}")
+    public ResponseEntity addBuilding(
+            @PathVariable long residentId,
+            @PathVariable long residenceId
+    ){
+        Resident resident =(Resident) userService.findOne(residentId);
+        if (resident == null)
+            return new ResponseEntity("User not found",HttpStatus.NOT_FOUND);
+
+        Residence residence = residenceService.findOneById(residenceId);
+        if (residence == null)
+            return new ResponseEntity("Residence not found", HttpStatus.NOT_FOUND);
+
+        residence.getResidents().add(resident);
+        resident.getResidences().add(residence);
+        userService.save(resident);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
