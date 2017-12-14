64b2a0cdf146d92a9e281bd6a387094f5dd886e2
sirko <diskozub@hotmail.com>
2017-12-14 10:26:39 +0100

    create announcement from meeting

--- src/main/java/com/timsedam/buildingmanagement/service/AnnouncementService.java
+++ src/main/java/com/timsedam/buildingmanagement/service/AnnouncementService.java
@@ -2,6 +2,7 @@ package com.timsedam.buildingmanagement.service;
 
 import com.timsedam.buildingmanagement.model.Announcement;
 import com.timsedam.buildingmanagement.model.Building;
+import com.timsedam.buildingmanagement.model.Meeting;
 import com.timsedam.buildingmanagement.model.Report;
 import com.timsedam.buildingmanagement.repository.AnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,19 @@ public class AnnouncementService {
         try{
             Announcement announcement= new Announcement(
                     report.getDescription(), LocalDateTime.now(),report.getSender(),report.getLocation()
+            );
+            return announcementRepository.save(announcement);
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Announcement createFromMeeting(Meeting meeting){
+        try{
+            String content="New meeting announced at "+meeting.getStartsAt().toString();
+            Announcement announcement= new Announcement(
+                    content, LocalDateTime.now(),null,meeting.getBuilding()
             );
             return announcementRepository.save(announcement);
         }catch(Exception e){
