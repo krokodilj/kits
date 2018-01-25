ce5d64ca3ea5280f09cebacb0566d90950ce5fb9
mladen <diskozub@hotmail.com>
2018-01-25 10:22:12 +0100

    nesto

---
+++ client/src/app/app.controller.js
@@ -0,0 +1,11 @@
+(function(){
+	angular.
+		module('kits')
+		.controller('AppController',appController);
+
+	function appController(){
+		//ovde na scope podatke o sesiji
+		//da moze da se korisi iz html templejta
+	}
+
+});
