3922e0578c46a1e88d7c5cc9e44a570c68e427e7
mladen <diskozub@hotmail.com>
2018-01-29 09:10:37 +0100

    create residence

--- client/src/app/core/models/residenceService.js
+++ client/src/app/core/models/residenceService.js
@@ -6,12 +6,24 @@
 		function residenceService($http){
 
 			return {
-				getByBuilding : getByBuilding
+				getByBuilding : getByBuilding,
+				create	: 	create
 			}
 
 			function getByBuilding(building_id){
 				var promise = $http
 								.get('/api/residences/by_building/'+building_id)
+								.then(function(response){
+									return {data:response.data}
+								},function(error){
+									return { error :true , data :error.data}
+								})
+				return promise
+			}
+
+			function create(residence){
+				var promise = $http
+								.post('/api/residences/',residence)
 								.then(function(response){
 									return {data:response.data}
 								},function(error){
---
+++ client/src/app/main/create_residence/create_residence.ctrl.js
@@ -0,0 +1,41 @@
+(function(){
+	angular
+		.module('kits.create_residence',['model.service.residence','model.service.building'])
+		.config(['$routeProvider',function($routeProvider){
+			$routeProvider
+				.when('/create_residence',{
+					templateUrl:'./app/main/create_residence/create_residence.html',
+					controller: 'CreateResidenceController',
+					controllerAs : 'vm'
+				})
+		}])
+		.controller('CreateResidenceController',['residenceService','buildingService','toastr','$location',
+			function(residenceService,buildingService,toastr,$location){
+
+				var vm = this
+
+				vm.residence={}
+
+				vm.create = create
+				buildingService.getAll()
+					.then(function(response){
+						if(response.error){
+							toastr.error("Unable to fetch buildings")
+						}else{
+							vm.buildings=response.data
+						}
+					})
+
+				function create(residence){
+					residenceService.create(residence)
+					.then(function(response){
+						if(response.error){
+							toastr.error("Error creating residence")
+						}else{
+							vm.residence={}
+							$location.path('/residence/'+response.data)
+						}
+					})
+				}
+		}])
+})();
